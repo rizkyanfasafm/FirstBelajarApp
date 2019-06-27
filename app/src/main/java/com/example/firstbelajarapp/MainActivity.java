52b99dfbd31f2cdb7a8d06332bd3a2ef42cb858c
Rizky Anfasa Farras Mada <rizkyanfasafm@gmail.com>
2019-06-27 12:30:06 +0700

    Initial commit

---
+++ app/src/main/java/com/example/firstbelajarapp/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.firstbelajarapp;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+//    ImageView iconpicture;
+//    TextView labelcheck;
+
+    ImageView applogo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        applogo = findViewById(R.id.applogo);
+
+//        applogo.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+////                menuju halaman lain
+//                Intent abc = new Intent(MainActivity.this, GetStartedAct.class);
+//                startActivity(abc);
+//                finish();
+//            }
+//        });
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent abc = new Intent(MainActivity.this, GetStartedAct.class);
+                startActivity(abc);
+                finish();
+            }
+        },2000);
+
+//        iconpicture = findViewById(R.id.iconpicture);
+//        labelcheck = findViewById(R.id.labelcheck);
+//
+////        Merubah text
+//        labelcheck.setText("haii dunia");
+//        labelcheck.setTextColor(Color.parseColor("#3456f0"));
+//
+////        Merubah warna
+//        iconpicture.setBackgroundColor(Color.parseColor("#FFFFC107"));
+    }
+}
